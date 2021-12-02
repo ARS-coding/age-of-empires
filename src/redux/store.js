dc8afa9d976ee9aace903b821f74b2843f3a047c
Ali Rıza Şahin <riza.sahin@getir.com>
2021-12-02 03:37:50 +0300

    Implement the saga to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,16 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import unitsReducer from "./slices/unitsSlice";
+import unitsSaga from "./sagas/unitsSaga";
 
-const middleware = [thunkMiddleware];
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [thunkMiddleware, sagaMiddleware];
 const composedEnhacer = composeWithDevTools(applyMiddleware(...middleware));
 const store = createStore(unitsReducer, composedEnhacer);
+
+sagaMiddleware.run(unitsSaga);
 
 export default store;
